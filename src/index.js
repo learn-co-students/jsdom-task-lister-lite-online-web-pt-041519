217fb671e42079bb95efdb3b4ed1f8a8ada4cc70
Dan Sullivan <dansullivan19@gmail.com>
2019-11-01 18:55:15 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+
+//prevents form from trying to POST somewhere
+document.getElementById("create-task-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+}, false);
+
+
+// runs addtask function when submitting task
+document.getElementById("create-task-form").addEventListener("submit", addTask);
+
+
+// finds ul#tasks, grabs entered value from form field new-task-description, creates a new li and TextNode and appends them to ul#tasks
+function addTask() {
+  let list = document.getElementById('tasks');
+  let task = document.getElementById('new-task-description').value;
+  // let task = document.querySelector('input#new-task-description').value;
+  let li = document.createElement('li');
+  li.appendChild(document.createTextNode(task));
+  list.appendChild(li);
+}
+
+
