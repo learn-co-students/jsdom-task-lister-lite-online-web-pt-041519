41684deb4e7c8b54f9a3dcd09092ea714ce612a4
CindyChenx <chen_cx0128@hotmail.com>
2019-11-01 21:21:41 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -13,17 +13,31 @@ document.addEventListener("DOMContentLoaded", () => {
     // slap newTask value on DOM
 
     // method 1: innerText
-    // const taskItems = document.createElement("li");
-    // taskItems.innerText = newTask;
+    const taskItems = document.createElement("li");
+    taskItems.innerText = newTask;
     // console.log(taskItems.innerText);
-    // taskList.appendChild(taskItems);
-    // taskForm.reset();
-
-    // method 2: innerHTML hardcode
-    taskList.innerHTML += `<li>${newTask}</li>`;
+    taskList.appendChild(taskItems);
     taskForm.reset();
 
+    // method 2: innerHTML hardcode
+    // taskList.innerHTML += `<li>${newTask}</li>`;
+    // taskForm.reset();
+
+    // add delete button
+    appendDeleteButton(taskItems);
+
   });
+
+  function appendDeleteButton(taskItems){
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "delete";
+    taskItems.appendChild(deleteButton);
+
+    deleteButton.addEventListener("click", function(e){
+      // console.log(e.target.parentElement);
+        e.target.parentElement.remove();
+    });
+  }
 
 
 });
