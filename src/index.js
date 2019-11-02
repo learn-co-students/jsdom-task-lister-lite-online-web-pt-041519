f61e25a65d7a29f172294bc921402ae720128231
CindyChenx <chen_cx0128@hotmail.com>
2019-11-01 20:56:35 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.querySelector("#create-task-form");
+  const taskList = document.querySelector("#tasks");
+  // console.log(taskForm);
+  // console.log(taskList);
+
+  taskForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    // console.log(e.target);
+    const newTask = e.target.querySelector("#new-task-description").value;
+    // console.log(newTask);
+    // slap newTask value on DOM
+
+    // method 1: innerText
+    // const taskItems = document.createElement("li");
+    // taskItems.innerText = newTask;
+    // console.log(taskItems.innerText);
+    // taskList.appendChild(taskItems);
+    // taskForm.reset();
+
+    // method 2: innerHTML hardcode
+    taskList.innerHTML += `<li>${newTask}</li>`;
+    taskForm.reset();
+
+  });
+
+
 });
