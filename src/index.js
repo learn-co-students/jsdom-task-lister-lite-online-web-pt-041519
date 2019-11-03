45f93f3396e9a5babc5674ef73eafdcd62f5e6b5
Michael Chen <michaelchengaming@gmail.com>
2019-11-03 01:21:00 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  let input = document.getElementById('new-task-description')
+  let form = document.querySelector('form')
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    
+    let ul = document.getElementById('tasks')
+    let li = document.createElement('li')
+
+    li.innerHTML = input.value
+    ul.appendChild(li)
+    event.target.reset()
+  })
+
+
+  // To celebrate success
+  const celebration = document.createElement('h1')
+  celebration.innerText = "WUBBA LUBBA DUB DUB!"
+  const main = document.getElementById('main-content')
+  main.appendChild(celebration)
+  main.insertBefore(celebration, main.querySelector('h1'))
 });
