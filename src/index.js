89d5f93f27a159e98b2ff1b48a9ad2a70b2c081d
Michael Chen <michaelchengaming@gmail.com>
2019-11-03 01:52:29 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 
+
+  // grabs elements and selectors
   let input = document.getElementById('new-task-description')
   let form = document.querySelector('form')
+  let submitButton = document.querySelector('input[type=submit]:nth-child(3)')
 
-  form.addEventListener("submit", (event) => {
-    event.preventDefault()
-    
-    let ul = document.getElementById('tasks')
-    let li = document.createElement('li')
 
-    li.innerHTML = input.value
-    ul.appendChild(li)
-    event.target.reset()
+ // Success alert
+  submitButton.addEventListener('click', (e) => {
+    alert("Wubba Lubba Dub Dub!")
   })
 
 
-  // To celebrate success
+  // stuff that happens when submit is triggered
+  form.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    // selects the ul and creates li 
+    let ul = document.getElementById('tasks')
+    let li = document.createElement('li')
+
+    // give the inner text for li whatever we enter as input and append it
+    li.innerHTML = input.value
+    ul.appendChild(li)
+    // sets the input field back to empty
+    input.value = ''
+  })
+
+
+  // Give app a better title
   const celebration = document.createElement('h1')
   celebration.innerText = "WUBBA LUBBA DUB DUB!"
   const main = document.getElementById('main-content')
