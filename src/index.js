70cadfafe5b76c0e0e4571fe033b3263a82443b2
github username <github email address>
2019-11-05 06:39:10 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+
+  // input fields
+  const form = document.getElementById("create-task-form");
+  const toDo = document.getElementById("new-task-description")
+
+  function addTaskToList(e) {
+    // add toDo string 
+    let list = document.getElementById("tasks")
+    let li = document.createElement('li');
+    
+    if ( toDo.value.length !== 0 ) {
+        li.innerText = toDo.value;
+        list.appendChild(li);
+    } 
+
+    // prevent submit event from performing its default behavior (submitting the form)
+    e.preventDefault();
+    e.target.reset();
+};
+
+
+// listen for submit
+form.addEventListener('submit', addTaskToList);   
 });
+
