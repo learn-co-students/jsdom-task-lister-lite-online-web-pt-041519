cf79916d44e2fe3c1230cf019d22922030679860
github username <github email address>
2019-11-07 18:47:32 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  const form = document.getElementById("create-task-form")
+  const toDo = document.getElementById("new-task-description")
+
+  function addTaskToLost(task) {
+    let list = document.getElementById("tasks")
+    let li = document.createElement("li")
+
+    if ( toDo.value.length !== 0 ) {
+      li.innerText = toDo.value
+      list.appendChild(li)
+    }
+    task.preventDefault()
+    task.target.reset()
+  }
+
+  form.addEventListener('submit', addTaskToList)
 });
