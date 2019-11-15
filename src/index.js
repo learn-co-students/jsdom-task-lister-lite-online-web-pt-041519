cf8ac936e89d74a674b9a85f2eb7204a5e6090d9
github username <github email address>
2019-11-14 21:15:03 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let input = document.querySelector('input#new-task-description')
+
+    let ul = document.querySelector('ul#tasks')
+
+    let newTask = document.createElement('li')
+    newTask.textContent = input.value 
+  })
 });
