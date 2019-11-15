a2cd17a1a02e41572a816499ea526f61c1ba32a4
github username <github email address>
2019-11-14 21:44:03 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Set variables for the form, input, and ul
   let form = document.querySelector('form')
   let input = document.querySelector('input#new-task-description')
   let ul = document.querySelector('ul#tasks')
   
   form.addEventListener('submit', (e) => {
     e.preventDefault()
-
+    // Create new li, set to input value, and append to the ul
     let newTask = document.createElement('li')
     newTask.innerText = input.value
     ul.appendChild(newTask)
-
+    // Clear the input field
     input.value = ''
   })
+
+  // Create and set up delete button
+  let deleteBtn = document.createElement('button')
+  deleteBtn.innerText = 'X'
+  deleteBtn.className = 'delete-button'
+  newTask.appendChild(deleteBtn)
+
+  deleteBtn.addEventListener('click', (e) => e.target.parentNode.remove())
 });
