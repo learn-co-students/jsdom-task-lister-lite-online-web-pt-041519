7f361a7dc4ca34aff25d351a9e0daba95ebcce65
helloimiskra <ibatistapoblete@gmail.com>
2019-12-11 17:38:16 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
   // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let input = document.querySelector('input#new-task-description')
+    
+    let ul = document.querySelector('ul')
+
+    let text = document.createElement("text")
+    
+    text.innerText = input.value + " "
+
+    let button = document.createElement("button")
+    button.textContent = "x"
+    button.addEventListener('click', (e) => e.target.parentNode.remove())
+
+    let li = document.createElement('li')
+    li.appendChild(text)
+    li.appendChild(button)
+    ul.appendChild(li)
+
+    input.value = " "
+    });
+  })
+
+
